16940f2cb463f99abac03024754c0d739405b8db
Argg1025 <agoddard@spartaglobal.com>
2019-01-13 15:14:16 +0000

    added a separate paths which a determined by a previous decision

--- main.js
+++ main.js
@@ -31,16 +31,51 @@ function controlFlow() {
 
     // ====CONTROL 3====
     var decision = ""
-    while (name == false) {
+    while (decision !== "yes" || decision !== "no") {
         var decision = prompt("Will you follow me?");
         if (decision === "yes") {
             console.log(`You are wise beyond your years ${name}. Your journey has only just begun.`)
+            break
         } else if (decision === "no") {
-            console.log("Coward! Be gone from here!")
-            return
+            console.log(alert("Coward!"))
+            break
         } else {
             console.log(alert("That is not an acceptable answer."))
         }
     }
+    // ====YES DECISION===
+    var weapon = "";
+    var punishment = "";
+    if (decision === "yes") {
+        while (weapon !== "sword" || decision !== "axe") {
+            var weapon = prompt("To begin your adventure, you must choose a weapon. Would you like an axe or a sword?");
+            if (weapon === "axe") {
+                console.log(alert(`Here is your ${weapon}. Now begin your quest.`))
+                return
+            } else if (weapon === "sword") {
+                console.log(alert(`Here is your ${weapon}. Now begin your quest.`))
+                return
+            } else {
+                console.log(alert("That is not an option."))
+            }
+        }
+    } else if (decision === "no") {
+        while (punishment !== "prison" || punishment !== "banished") {
+            var punishment = prompt("You will now face the consequences! Will you face prison or will you be banished from this land?");
+            if (punishment === "prison") {
+                console.log(alert(`You are now going to ${punishment}.`))
+                return
+            } else if (punishment === "banished") {
+                console.log(alert(`You will now be ${punishment}.`))
+                return
+            } else {
+                console.log(alert("Choose now or face an even worse fate."))
+            }
+        }
+    }
+
+
+
 }
+
 console.log(controlFlow())
